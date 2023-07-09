65382d543d6fff6a571008408d30c05a51ed3ccc
leen-neel <and24903@gmail.com>
2023-07-10 00:06:09 +0530

    now page redirects to login and scan page - NOW POLISHED

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,7 @@ const routes: RouteRecordRaw[] = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: '/scan', component: () => import('pages/ScanQr.vue') },
+      { path: '/login', component: () => import('pages/LoginPage.vue') },
     ],
   },
 
