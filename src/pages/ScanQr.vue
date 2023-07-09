39ba92482d9d7c054a9eb87def2b094ba78a78f6
leen-neel <and24903@gmail.com>
2023-07-09 21:32:30 +0530

    added qrcode page

--- src/pages/ScanQr.vue
+++ src/pages/ScanQr.vue
@@ -1,1 +1,44 @@
-<template>scan dat ass bbg</template>
+<template>
+  <div>
+    <p class="error">{{ error }}</p>
+
+    {{ store.store?.name }}
+    {{ result }}
+
+    <qrcode-stream @decode="onDecode" @init="onInit" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { QrcodeStream } from 'vue-qrcode-reader';
+
+import { currentStore } from 'stores/whatStoreStore';
+
+// data
+const result = ref<Store>();
+const error = ref('');
+const store = currentStore();
+
+type Store = {
+  name: string;
+  photoURL: string;
+};
+
+// methods
+const onDecode = (data: Store) => {
+  result.value = data;
+  store.joinStore(data);
+};
+
+const onInit = async () => {
+  console.log('whatever');
+};
+</script>
+
+<style scoped>
+.error {
+  font-weight: bold;
+  color: red;
+}
+</style>
