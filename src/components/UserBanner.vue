c0dacbfef3a408046d969276797e564edb5fd78d
leen-neel <and24903@gmail.com>
2023-07-09 17:25:28 +0530

    moved banner to new component

---
+++ src/components/UserBanner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex items-end gap-2 banner q-pa-md q-mt-lg q-mx-sm">
+    <q-avatar size="50px">
+      <q-img
+        :src="user.user?.photoURL!"
+        alt="test"
+        referrerpolicy="no-referrer"
+      />
+    </q-avatar>
+
+    <h2 class="text-h6 q-ma-none q-ml-sm text-bold text-white">
+      Welcome {{ user.user?.displayName }}!
+    </h2>
+  </div>
+
+  <q-btn
+    color="white"
+    text-color="primary"
+    :label="user.user?.uid ? 'Logout' : 'Login'"
+    :icon="user.user?.uid ? 'logout' : 'login'"
+    class="q-ma-none"
+    @click="handleLogin()"
+  />
+</template>
+
+<script setup lang="ts">
+import { userStore } from 'stores/userStore';
+const user = userStore();
+
+const handleLogin = () => {
+  user.user?.uid ? user.logout() : user.login();
+};
+</script>
+
+<style lang="scss">
+.banner {
+  border-radius: 16px;
+  background: linear-gradient(226deg, #0284c7 0%, #10b981 82.09%);
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,6 @@
 <template>
   <q-page class="q-pa-sm">
-    <div class="flex items-end gap-2 banner q-pa-md q-mt-lg q-mx-sm">
-      <q-avatar size="50px">
-        <q-img
-          :src="user.user?.photoURL!"
-          alt="test"
-          referrerpolicy="no-referrer"
-        />
-      </q-avatar>
-
-      <h2 class="text-h6 q-ma-none q-ml-sm text-bold text-white">
-        Welcome {{ user.user?.displayName }}!
-      </h2>
-    </div>
-
-    <q-btn
-      color="white"
-      text-color="primary"
-      :label="user.user?.uid ? 'Logout' : 'Login'"
-      :icon="user.user?.uid ? 'logout' : 'login'"
-      class="q-ma-none"
-      @click="handleLogin()"
-    />
+    <Banner />
 
     <div class="flex items-end q-mx-sm q-mt-md">
       <q-avatar size="50px">
@@ -48,25 +27,20 @@ import { onMounted, ref } from 'vue';
 import { generateQR } from 'boot/qrcode';
 import { userStore } from 'stores/userStore';
 
+import Banner from 'components/UserBanner.vue';
 import CartItems from 'components/CartItem.vue';
 
 const user = userStore();
 let qr = ref('');
 
 onMounted(async () => {
-  let qrGen = await generateQR('gay sex').then((data) => {
+  let data = {
+    name: 'Balls',
+    price: 3,
+  };
+
+  let qrGen = await generateQR(JSON.stringify(data)).then((data) => {
     qr.value = data;
   });
 });
-
-const handleLogin = () => {
-  user.user?.uid ? user.logout() : user.login();
-};
 </script>
-
-<style lang="scss">
-.banner {
-  border-radius: 16px;
-  background: linear-gradient(226deg, #0284c7 0%, #10b981 82.09%);
-}
-</style>
