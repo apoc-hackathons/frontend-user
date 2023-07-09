d6522633c9f4fb41dc20752b82b739187fd9e0e8
leen-neel <and24903@gmail.com>
2023-07-09 13:27:50 +0530

    added auth; base design on banner

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,50 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page padding>
+    <div class="flex items-end gap-2 banner q-pa-md">
+      <q-avatar size="70px">
+        <q-img
+          :src="user.user.photoURL"
+          alt="test"
+          referrerpolicy="no-referrer"
+        />
+      </q-avatar>
+
+      <h2 class="text-h3 q-ma-none q-mx-sm text-bold">
+        Welcome {{ user.user.displayName }} !
+      </h2>
+      <q-btn
+        color="white"
+        text-color="primary"
+        label="Login"
+        icon="login"
+        class="q-ma-none"
+        @click="user.login()"
+      />
+    </div>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
+import { userStore } from 'stores/userStore';
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  setup() {
+    const user = userStore();
+
+    return {
+      user,
+    };
   },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
 });
 </script>
+
+<style lang="scss">
+.banner {
+  border-radius: 16px;
+  background: linear-gradient(226deg, #0284c7 0%, #10b981 82.09%);
+}
+</style>
